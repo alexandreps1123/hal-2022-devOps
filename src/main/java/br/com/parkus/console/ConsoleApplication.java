d70d8a550acd5b2aed12fb8b825d5e5aa1d589f1
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-08-26 10:36:28 -0300

    dividido o start executa  build e run, adicionado entrada de dados via teclado

--- src/main/java/br/com/parkus/console/ConsoleApplication.java
+++ src/main/java/br/com/parkus/console/ConsoleApplication.java
@@ -1,14 +1,45 @@
 package br.com.parkus.console;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsoleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		System.out.println("Console log");
+
+		Scanner teclado = new Scanner(System.in);
+		String nome;
+		
+		System.out.println("Insira se nome:");
+
+		nome = teclado.nextLine();
+
+		System.out.println("Seu nome: "+nome);
+		
+		/**
+		// Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+ 
+        // Reading data using readLine
+        String name = reader.readLine();
+ 
+        // Printing the read line
+        System.out.println(name);
+		
+		
+		reader.close();
+ 		*/
+
 		// SpringApplication.run(ConsoleApplication.class, args);
 	}
 
