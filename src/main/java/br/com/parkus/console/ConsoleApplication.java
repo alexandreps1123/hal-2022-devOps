69341d1f1a7fca9758892e736d3db7944f96541e
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-08-26 09:21:41 -0300

    inicialziacao do projeto devOps

---
+++ src/main/java/br/com/parkus/console/ConsoleApplication.java
@@ -0,0 +1,15 @@
+package br.com.parkus.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Console log");
+		// SpringApplication.run(ConsoleApplication.class, args);
+	}
+
+}
